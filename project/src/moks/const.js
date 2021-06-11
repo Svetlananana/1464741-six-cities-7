8cf23ddc8edda0adb61ff91f7990f1e10db6992e
poison_ivy <hellosvetasmirnova@gmail.com>
2021-06-11 14:28:18 +0300

    Создала моки для предложений по аренде

---
+++ project/src/moks/const.js
@@ -0,0 +1,59 @@
+export const HOTEL_CITIES = [
+  'Amsterdam',
+  'Paris',
+  'Cologne',
+  'Brussels',
+  'Hamburg',
+  'Dusseldorf',
+];
+
+export const HOTEL_GOODS = [
+  'Heating',
+  'Kitchen',
+  'Cable TV',
+  'Washing machine',
+  'Coffee machine',
+  'Dishwasher',
+];
+
+export const HOTEL_HOST_NAMES = [
+  'Angelina',
+  'Max',
+];
+
+export const HOTEL_TYPE = [
+  'Apartment',
+  'Private room',
+  'House',
+  'Hotel',
+];
+
+export const HOTEL_TITLE = [
+  'Nice, cozy, warm big bed apartment',
+  'Wood and stone place',
+  'Beautiful &amp; luxurious apartment at great location',
+  'Canal View Prinsengracht',
+];
+
+export const HOTEL_DESCRIPTIONS = [
+  'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
+  'An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.',
+];
+
+export const HOTEL_HOST_AVATAR = [
+  './img/avatar-angelina.jpg',
+  './img/avatar-max.jpg',
+];
+
+export const HOTEL_IMAGES = [
+  './img/apartment-01.jpg',
+  './img/apartment-02.jpg',
+  './img/apartment-03.jpg',
+  './img/room.jpg',
+];
+
+export const HOTEL_PREVIEW_IMAGES = [
+  './img/apartment-small-03.jpg',
+  './img/apartment-small-04.jpg',
+  './img/room-small.jpg',
+];
