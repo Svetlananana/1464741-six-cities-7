e2f350333367fa9147440c623ab1ddadeee44f91
poison_ivy <hellosvetasmirnova@gmail.com>
2021-07-04 02:43:16 +0300

    Добаила компонент с картой и хук useMap

---
+++ project/src/hooks/useMap.js
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react';
+import L from 'leaflet';
+
+export default function useMap(mapRef, city) {
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    const {latitude, longitude, zoom} = city.location;
+
+    if (mapRef.current !== null && map === null) {
+      const instance = L.map(mapRef.current, {
+        center: {
+          lat: latitude,
+          lng: longitude,
+        },
+        zoom: zoom,
+        marker: true,
+      });
+
+      L.tileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        },
+      )
+        .addTo(instance);
+
+      setMap(instance);
+    }
+  }, [mapRef, map, city]);
+
+  return map;
+}
