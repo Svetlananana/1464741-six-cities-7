2bef7178385a4b7c104df3182158d3b7bd1cd2a5
poison_ivy <hellosvetasmirnova@gmail.com>
2021-07-08 17:42:34 +0300

    Добавила коммнтерии и отображение нужного количества карточек предложений

--- project/src/components/pages/room-screen/room-screen.jsx
+++ project/src/components/pages/room-screen/room-screen.jsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
 import {Redirect} from 'react-router-dom';
 import {useParams} from 'react-router-dom';
-import {propOffersTypes} from '../../../type-props';
+import {propOffersTypes, propReviewTypes} from '../../../type-props';
 import {formatRating} from '../../../moks/utils';
 import PropTypes from 'prop-types';
 import {FormReviews} from '../../form-reviews/form-reviews';
 import Header from '../../header/header';
 import OffersList from '../../offer-list/offer-list';
 import Map from '../../map/map';
+import Reviews from '../../reviews/reviews';
 
-export default function RoomScreen({ offers }) {
+export default function RoomScreen({ offers, reviews }) {
   const {id} = useParams();
 
   const room = offers.find((offer) => offer.id === +id);
@@ -27,18 +28,19 @@ export default function RoomScreen({ offers }) {
     images,
     host,
     description,
+    city,
   } = room;
 
   const {
-    // avatarUrl,
+    avatarUrl,
     isPro,
     name,
   } = host;
 
-  const [hoveredCard, setHoveredCard] = useState(room);
+  const [, setHoveredCard] = useState(room);
 
-  if (room === -1) {
-    return <Redirect to="/" />;
+  if (room === {}) {
+    return <Redirect to="/" />; // а как правильно сделать условие?
   }
 
   const nearPlacesCard = offers.filter((card) => card !== room).slice(0, 3);
@@ -116,7 +118,7 @@ export default function RoomScreen({ offers }) {
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
                   <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
+                    <img className="property__avatar user__avatar" src={avatarUrl} width="74" height="74" alt="Host avatar"/>
                   </div>
                   <span className="property__user-name">
                     {name}
@@ -133,51 +135,29 @@ export default function RoomScreen({ offers }) {
                 </div>
               </div>
               <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
-                      </div>
-                      <span className="reviews__user-name">
-                  Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: '80%'}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                  A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
-                </ul>
-                <FormReviews/>
+                <Reviews reviews={reviews} />
+                <FormReviews />
               </section>
             </div>
           </div>
           <section className="property__map map">
             <Map
-              city={offers[0].city}
-              offers={offers}
-              hoveredCard={hoveredCard}
+              city={city}
+              offers={nearPlacesCard}
+              hoveredCard={room}
             />
           </section>
         </section>
         <div className="container">
           <section className="near-places places">
             <h2 className="near-places__title">Other places in the neighbourhood</h2>
-            <OffersList
-              offers={nearPlacesCard}
-              onMouseEnter={onHoverCard}
-              onMouseLeave={() => setHoveredCard(room)}
-            />
+            <div className="near-places__list places__list">
+              <OffersList
+                offers={nearPlacesCard}
+                onMouseEnter={onHoverCard}
+                onMouseLeave={() => setHoveredCard(room)}
+              />
+            </div>
           </section>
         </div>
       </main>
@@ -189,4 +169,6 @@ RoomScreen.propTypes = {
   offers: PropTypes.arrayOf(
     PropTypes.shape(propOffersTypes),
   ),
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape(propReviewTypes).isRequired),
 };
