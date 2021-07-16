ec7cec8df876bc2099fef0f056ba2d47c9ec4250
poison_ivy <hellosvetasmirnova@gmail.com>
2021-07-17 01:00:17 +0300

    Инициализировала новое хранилище

--- project/src/index.js
+++ project/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {reducer} from './store/reducer';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
+
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
 
