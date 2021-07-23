84c700f67c8ed0df1b97d6d1f13ae1a15240c7a4
poison_ivy <hellosvetasmirnova@gmail.com>
2021-07-23 17:35:52 +0300

    Рефакторинг

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,23 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {AppRoutes} from '../../const';
 import MainPageScreen from '../pages/main-page-screen/main-page-screen';
 import LoginScreen from '../pages/login-screen/login-screen';
 import FavoritesScreen from '../pages/favorites-screen/favorites-screen';
 import RoomScreen from '../pages/room-screen/room-screen';
 import NotFoundScreen from '../pages/not-found-screen/not-found-screen';
+import LoadingScreen from '../pages/loading-screen/loading-screen';
+import {AuthorizationStatus} from '../../const';
 
-export default function App() {
+const isCheckedAuth = (authorizationStatus) => authorizationStatus === AuthorizationStatus.UNKNOWN;
+
+export function App({authorizationStatus, isDataLoaded}) {
+
+  if (isCheckedAuth(authorizationStatus) || !isDataLoaded) { // какую из этих проверок лучше оставить?
+    return (
+      <LoadingScreen />
+    );
+  }
+  // export function App({isDataLoaded}) {
+
+  //   if (!isDataLoaded) {
+  //     return (
+  //       <LoadingScreen />
+  //     );
+  //   }
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={AppRoute.MAIN} component={MainPageScreen} />
-        <Route exact path={AppRoute.LOGIN} component={LoginScreen} />
-        <Route exact path={AppRoute.FAVORITES} component={FavoritesScreen} />
-        <Route exact path={AppRoute.ROOM} component={RoomScreen} />
+        <Route exact path={AppRoutes.MAIN} component={MainPageScreen} />
+        <Route exact path={AppRoutes.LOGIN} component={LoginScreen} />
+        <Route exact path={AppRoutes.FAVORITES} component={FavoritesScreen} />
+        <Route exact path={AppRoutes.ROOM} component={RoomScreen} />
         <Route component={NotFoundScreen} />
       </Switch>
     </BrowserRouter>
   );
 }
+
+
+App.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  isDataLoaded: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+  isDataLoaded: state.isDataLoaded,
+});
+
+export default connect(mapStateToProps, null)(App);
