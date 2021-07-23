1ccb86d17cd7071c992188a7401e3cf882b3ca1d
poison_ivy <hellosvetasmirnova@gmail.com>
2021-07-23 17:29:14 +0300

    Написала код для загрузки и отрисовки данных с сервера

---
+++ project/src/store/api-actions.js
@@ -0,0 +1,28 @@
+import {ActionCreator} from './action';
+import {AuthorizationStatus, APIRoutes} from '../const';
+import {adaptOffersToClient} from '../adapter/adapter';
+
+export const fetchOffers = () => (dispatch, getState, api) => {
+  api.get(APIRoutes.OFFERS)
+    .then(({ data }) => adaptOffersToClient(data))
+    .then((offers) => dispatch(ActionCreator.loadOffers(offers)));
+  // .then((offers) => console.log(offers));
+};
+
+export const checkAuth = () => (dispatch, getState, api) => (
+  api.get(APIRoutes.LOGIN)
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch(() => {})
+);
+
+export const login = ({login: email, password}) => (dispatch, getState, api) => (
+  api.post(APIRoutes.LOGIN, {email, password})
+    .then(({data}) => localStorage.setItem('token', data.token))
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+);
+
+export const logout = () => (dispatch, getState, api) => (
+  api.delete(APIRoutes.LOGOUT)
+    .then(() => localStorage.removeItem('token'))
+    .then(() => dispatch(ActionCreator.logout()))
+);
