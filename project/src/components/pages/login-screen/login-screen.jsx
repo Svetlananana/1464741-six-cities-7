30c1b9bb63ebac3c660b2de90c6f42610a11e5cf
poison_ivy <hellosvetasmirnova@gmail.com>
2021-07-25 04:54:11 +0300

    Добавила обработчик событий на отправку формы

--- project/src/components/pages/login-screen/login-screen.jsx
+++ project/src/components/pages/login-screen/login-screen.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
+// import {useHistory} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login} from '../../../store/api-actions';
+import {AppRoutes} from '../../../const';
 import {Link} from 'react-router-dom';
 import Header from '../../header/header';
 
-export default function LoginScreen() {
+export function LoginScreen({activeCity, onSubmit}) {
+
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  // const history = useHistory();
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  }
+
   return (
     <div className="page page--gray page--login">
       <Header/>
@@ -10,22 +30,22 @@ export default function LoginScreen() {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form onSubmit={handleSubmit} className="login__form form" action="#" method="post">
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required=""/>
+                <input ref={loginRef} className="login__input form__input" type="email" name="email" placeholder="Email" required=""/>
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required=""/>
+                <input ref={passwordRef} className="login__input form__input" type="password" name="password" placeholder="Password" required=""/>
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <Link className="locations__item-link" to="/">
-                <span>Amsterdam</span>
+              <Link className="locations__item-link" to={AppRoutes.MAIN}>
+                <span>{activeCity}</span>
               </Link>
             </div>
           </section>
@@ -34,3 +54,20 @@ export default function LoginScreen() {
     </div>
   );
 }
+
+LoginScreen.propTypes = {
+  activeCity: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  activeCity: state.activeCity,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
